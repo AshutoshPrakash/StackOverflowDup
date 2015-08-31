d672a7a3e290ed3c7719620528595a66d19ec11e
Ashutosh Prakash <prakash.ashutosh12@gmail.com>
2015-08-31 18:34:32 +0530

    find tag v1.0
    finds tag of a set of questions.
    Input : two files (1. stop words file , 2. set of question for queries) , stack overflow's "database tags"
    output : prints question and its corresponding tags.

---
+++ find_tag.py
@@ -0,0 +1,37 @@
+import MySQLdb
+
+db = MySQLdb.connect("localhost","root","ashu","stack_overflow" )
+cursor = db.cursor()
+cursor.execute("select name from tag;")
+data = cursor.fetchall()
+db.close()
+
+tags = [element for tupl in data for element in tupl] #flattening a nested structure
+
+
+with open("english_stop_words.txt") as f:
+    stop_words = f.readlines()
+stop_words = [line.rstrip('\n') for line in stop_words]
+
+with open("questions.txt") as f2:
+    questions = f2.readlines()
+    
+output_tags = []
+for q in questions:
+	#print "\n# Question found :",q
+	q2 = q.replace("\n","")
+	#splitting a string into list of words and removing special symbols
+	q = q.replace("\n","").replace(".","").replace("-","").replace("'","").replace("?","").lower().split()
+	
+	for s in stop_words:
+		if s.lower() in q:
+			q[:] = [item for item in q if item != s.lower()]#removes all instances
+	#print "- After stop_word removal :",q
+	ll = []
+	for q1 in q:
+		if q1 in tags:
+			#print "tag found : ",q1
+			ll.append(q1)
+	output_tags.append({'q':q2 ,'tags':ll})
+
+print output_tags
